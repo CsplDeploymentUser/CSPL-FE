144705796d49cdad643df31ebbd2e1c8c6ba2457
Adham Kazi <adhamul.kazi@consociatesolutions.com>
2024-10-17 15:42:03 +0530

    Components

---
+++ Components/ServerSideComponents/HeaderComponents/Header.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = async () => {
+  return <div>Hello</div>;
+};
+
+export default Header;
---
+++ Components/ServerSideComponents/HomePageComponents/HomePageAllComponents.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const HomePageAllComponents = (props: Props) => {
+  return <div>HomePageAllComponents</div>;
+};
+
+export default HomePageAllComponents;
